c573ec503da9aafa5c2e4151bc74c594dbe755c2
hantong <952587282@qq.com>
2017-11-01 10:02:34 +0800

    协程

---
+++ coroutine/gen.php
@@ -0,0 +1,20 @@
+<?php
+
+function gen()
+{
+    $ret = (yield 'yield1');
+    var_dump($ret);
+
+    $ret = (yield 'yield2');
+    var_dump($ret);
+}
+
+$gen = gen();
+
+var_dump($gen->current());
+
+var_dump($gen->next());
+
+var_dump($gen->send('ret1'));
+
+// var_dump($gen->send('ret2'));
